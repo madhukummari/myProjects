804220bd0a11d1e5bd496f4c71d66192aeee74dc
Madhu <madhu@MacBook-Air.local>
2025-07-29 23:57:45 +0530

    vault already exists error

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-resource "aws_backup_vault" "main" {
+resource "aws_backup_vault" "my_vault-terraform" {
   name = "my-backup-vault"
 }
 
-resource "aws_backup_plan" "main" {
+resource "aws_backup_plan" "mybackup_plan" {
   name = "ec2-ebs-backup-plan"
 
   rule {
