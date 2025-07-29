5de1f1d4ab7886a8ee04597e0307488c18ba9aa4
Madhu <madhu@MacBook-Air.local>
2025-07-29 21:06:17 +0530

    test terraform

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+resource "aws_backup_vault" "main" {
+  name = "my-backup-vault"
+}
+
+resource "aws_backup_plan" "main" {
+  name = "ec2-ebs-backup-plan"
+
+  rule {
+    rule_name         = "6-hour-snapshot"
+    target_vault_name = aws_backup_vault.main.name
+    schedule          = "cron(0 */6 * * ? *)"
+    lifecycle {
+      delete_after = 7
+    }
+  }
+}
+
+resource "aws_backup_selection" "tag_selection" {
+  iam_role_arn = "arn:aws:iam::ACCOUNT_ID:role/service-role/AWSBackupDefaultServiceRole"
+  name         = "backup-tagged-resources"
+  plan_id      = aws_backup_plan.main.id
+
+  selection_tag {
+    type  = "STRINGEQUALS"
+    key   = "Backup"
+    value = "true"
+  }
+}
