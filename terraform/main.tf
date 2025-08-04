f5d7c4ef523df6e2704189bb6bfba00c3c4914b6
Madhu <madhu@MacBook-Air.local>
2025-08-04 19:38:37 +0530

    creating ec2 instnces

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,39 @@ resource "aws_backup_selection" "tag_selection" {
     value = "true"
   }
 }
+
+###################################### Phase 2 : ec2 and volume creation ######################################
+resource "aws_instance" "web" {
+  ami           = "ami-020cba7c55df1f615" # Amazon Linux 2
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              mkfs -t ext4 /dev/xvdf
+              mkdir /data
+              mount /dev/xvdf /data
+              echo "Mounted volume and created directory" > /data/setup.txt
+              EOF
+
+  tags = {
+    Name   = "Backup-Instance"
+    Backup = "true"
+  }
+}
+
+resource "aws_ebs_volume" "additional_volume" {
+  availability_zone = aws_instance.web.availability_zone
+  size              = 8 # Size in GB
+
+  tags = {
+    Name   = "Backup-Volume"
+    Backup = "true"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = "/dev/xvdf"
+  volume_id   = aws_ebs_volume.additional_volume.id
+  instance_id = aws_instance.web.id
+}
